0e5843abda34f5c3d7f2ea3d14189eef1b22b872
Chase <sde.chase.rogers@gmail.com>
2021-08-25 11:15:38 -0600

    made changes to nav

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,32 +1,39 @@
 import React from 'react';
 
 function Landing() {
+
+    const onChangeHandler = () => {}
+
+    const submitHandler = () => {}
+
     return (
         <div className='Landing'>
-            <>
+            <div className='darkenImage'>
+
             <p>Find your dream job</p>
             <p>Pick a career that doesn't need escaping from!</p>
-            <form className='LandingForm' onSubmit={''}>
-                <input
-                    name=""
-                    value={''}
-                    onChange={''}
-                    type=""
-                    placeholder="Key Words"
-                />
-                <input
-                    name=""
-                    value={''}
-                    onChange={''}
-                    type=""
-                    placeholder="Location"
-                />
-                <button type="submit">
-                    Search
-                </button>
+            <form className='LandingForm' onSubmit={submitHandler}>
+                    <input
+                        name=""
+                        value={''}
+                        onChange={onChangeHandler}
+                        type=""
+                        placeholder="Key Words"
+                    />
+                    <input
+                        name=""
+                        value={''}
+                        onChange={onChangeHandler}
+                        type=""
+                        placeholder="Location"
+                    />
+                    <button type="submit">
+                        Search
+                    </button>
+                
             </form>
             <a></a>
-            </>
+            </div>
         </div>
     )
 }
